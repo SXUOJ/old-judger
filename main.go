adb0f2d73fa9c92c1c4444e367b01d4e81b53d6d
isther <28251536@qq.com>
2022-03-15 11:31:55 +0800

    Add a api to submit

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/Sxu-Online-Judge/judger/model"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +12,28 @@ func setupRouter() *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.POST("/submit", func(c *gin.Context) {
+		test := model.Submit{}
+		if err := c.ShouldBindJSON(&test); err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "bind model error",
+			})
+			return
+		}
+
+		//TODO:judge
+
+		c.JSON(http.StatusOK, gin.H{
+			"submit_id":        test.SubmitId,
+			"problem_id":       test.ProblemId,
+			"problem_type":     test.ProblemType,
+			"code_type":        test.CodeType,
+			"code_source_path": test.CodeSourcePath,
+			"time_limit":       test.TimeLimit,
+			"memory_limit":     test.MemoryLimit,
 		})
 	})
 	return r
