ffd7b8fccb894e2999b8947a3259e4b3ef4b87d5
isther <28251536@qq.com>
2022-03-15 11:23:53 +0800

    ping

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
 
 func main() {
-	fmt.Println("Hello Judger")
+	r := setupRouter()
+	r.Run()
 }
