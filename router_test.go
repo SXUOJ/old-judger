5ef535e25307a9e81ecaf836eb07ccb86a2fdca3
isther <28251536@qq.com>
2022-03-15 11:32:10 +0800

    submit test

--- router_test.go
+++ router_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Sxu-Online-Judge/judger/model"
 	"gotest.tools/assert"
 )
 
@@ -18,4 +22,28 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
+
+func TestSubmitRouteParameter(t *testing.T) {
+	router := setupRouter()
+
+	params := model.Submit{
+		SubmitId:       "1",
+		ProblemId:      "1",
+		ProblemType:    "1",
+		CodeType:       "c",
+		CodeSourcePath: "main.c",
+		Limit: model.Limit{
+			TimeLimit:   2000,
+			MemoryLimit: 256,
+		},
+	}
+
+	paramsByte, _ := json.Marshal(params)
+	req, _ := http.NewRequest("POST", "/submit", bytes.NewBuffer(paramsByte))
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	log.Println(w.Body.String())
+	assert.Equal(t, 200, w.Code)
 }
