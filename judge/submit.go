1b0d3572a4eeb05acb15624a13861398972d095b
isther <28251536@qq.com>
2022-03-21 15:15:18 +0800

    Modifed submit model

---
+++ judge/submit.go
@@ -0,0 +1,14 @@
+package judge
+
+type Submit struct {
+	SubmitId string `json:"submit_id"`
+
+	ProblemId   string `json:"problem_id"`
+	ProblemType string `json:"problem_type"`
+
+	CodeType       string `json:"code_type"`
+	CodeSourcePath string `json:"code_source_path"`
+
+	TimeLimit   string `json:"time_limit"`
+	MemoryLimit string `json:"memory_limit"`
+}
