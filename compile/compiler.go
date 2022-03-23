d026f0eb0c7c3777e10f15b4f50722557649d9aa
isther <28251536@qq.com>
2022-03-23 15:29:55 +0800

    Modifed CompileResult

--- compile/compiler.go
+++ compile/compiler.go
@@ -19,16 +19,8 @@ var (
 	ERROR_NOT_SUPPORT_LANG = errors.New("This language is not supported")
 )
 
-type CompileResult struct {
-	Status string `json:"status,omitempty"`
-
-	CpuTime  string `json:"cpu_time,omitempty"`
-	RealTime string `json:"real_time,omitempty"`
-	Memory   string `json:"memory,omitempty"`
-
-	Signal   string `json:"signal,omitempty"`
-	ErrorInf string `json:"msg,omitempty"`
-}
+// CompileResult
+type CompileResult model.Result
 
 type Compiler struct {
 	codeType string
