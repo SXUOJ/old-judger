146e2761644a1d77e146ce8334a85a78846e4d64
isther <28251536@qq.com>
2022-03-25 11:04:46 +0800

    Use in-package functions

--- judge/judger.go
+++ judge/judger.go
@@ -1,18 +1,17 @@
 package judge
 
 import (
-	"github.com/isther/judger/compile"
 	"github.com/isther/judger/model"
 )
 
 type Judger struct {
-	compile.Compiler
+	Compiler
 	Runner
 }
 
 func NewJudger(submit *model.Submit) *Judger {
 	return &Judger{
-		Compiler: *compile.NewCompiler(submit),
+		Compiler: *newCompiler(submit),
 		Runner:   *newRunner(submit),
 	}
 }
