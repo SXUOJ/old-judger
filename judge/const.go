a51b2b753f90467e3f0f7a91b62fcf1a63c297bc
isther <28251536@qq.com>
2022-03-25 11:08:41 +0800

    Merge two file

--- judge/const.go
+++ judge/const.go
@@ -13,3 +13,34 @@ const (
 	LOOKUP_FAILED
 	COMPILE_FAILED
 )
+
+const (
+	StatusAC  = 1
+	StatusWA  = 2
+	StatusCE  = 3
+	StatusRE  = 4
+	StatusTLE = 5
+	StatusMLE = 6
+	StatusOLE = 7
+	StatusPE  = 8
+	StatusSE  = 9
+)
+
+var judgeStatus = map[string]string{
+	"1": "Accepted",
+	"2": "Wrong Answer",
+	"3": "Compile Error",
+	"4": "Runtime Error",
+	"5": "Time Limit Exceed",
+	"6": "Memory Limit Exceed",
+	"7": "Output Limit Exceed",
+	"8": "Presentation Error",
+	"9": "System Error",
+}
+
+func GetJudgeStatus(result string) string {
+	if v, ok := judgeStatus[result]; ok {
+		return v
+	}
+	return "no"
+}
