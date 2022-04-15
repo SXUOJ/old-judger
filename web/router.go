991d49cfeefd47b97243881a97207496c5b02360
isther <28251536@qq.com>
2022-04-15 10:47:25 +0800

    add function

--- web/router.go
+++ web/router.go
@@ -1,23 +1,24 @@
 package web
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func loadRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
-	r.POST("/submit", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"result": "result",
-		})
-	})
+	r.GET("/ping", ping)
+	r.POST("/submit", submit)
+
 	return r
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "pong",
+	})
+}
+
+func submit(c *gin.Context) {
+
 }
